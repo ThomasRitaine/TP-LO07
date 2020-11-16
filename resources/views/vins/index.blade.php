3d83cd7959cef97317bbcabf37b7a483de8f3b64
Thomas Ritaine <thomas.ritaine@outlook.com>
2020-11-16 11:09:40 +0100

    Ex 2.5

--- resources/views/vins/index.blade.php
+++ resources/views/vins/index.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.app')
 @section('content')
 <h1>Liste des vins</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Cru</th>
+        <th scope="col">Annee</th>
+        <th scope="col">Degre</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($listeVins as $unVin)
+        <tr>
+            <th scope="row">{{ $unVin->id }}</th>
+            <td>{{ $unVin->cru }}</td>
+            <td>{{ $unVin->annee }}</td>
+            <td>{{ $unVin->degre }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+
 @endsection
