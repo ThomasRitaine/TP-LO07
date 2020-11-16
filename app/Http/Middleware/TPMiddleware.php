11f5d1fbe600b9ba046928a5fd0afde58fb516ac
Thomas Ritaine <thomas.ritaine@outlook.com>
2020-11-16 13:12:41 +0100

    Ex 6.4

--- app/Http/Middleware/TPMiddleware.php
+++ app/Http/Middleware/TPMiddleware.php
@@ -14,7 +14,11 @@ class TPMiddleware
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {
+{
+    if(now()->format('s') % 2) {
         return $next($request);
     }
+    dd("Requête bloquée par le middleware");
+}
+
 }
