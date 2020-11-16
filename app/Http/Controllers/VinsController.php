1130d077eeb527468b79a9be96176b3db454a3f4
Thomas Ritaine <thomas.ritaine@outlook.com>
2020-11-16 11:23:55 +0100

    Ex 3.6

--- app/Http/Controllers/VinsController.php
+++ app/Http/Controllers/VinsController.php
@@ -13,5 +13,14 @@ class VinsController extends Controller
         return view('vins.index', compact('listeVins'));
      }
 
+
+     private function validator() {
+        return request()->validate([
+            'cru' => 'required|min:1',
+            'annee' => 'required|integer',
+            'degre' => 'required|numeric|min:0|max:100'
+        ]);
+    }
+    
      
 }
